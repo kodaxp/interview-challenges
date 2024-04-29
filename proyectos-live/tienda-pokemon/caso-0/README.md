175e2d9a1fe988d65e66ef6d722fbd492eec9ebe
koda <joelzuniga@outlook.cl>
2024-04-29 19:45:52 -0400

    Pokemon shop case 0 resolved

--- proyectos-live/tienda-pokemon/caso-0/README.md
+++ proyectos-live/tienda-pokemon/caso-0/README.md
@@ -1,6 +1,6 @@
 # Ejercicios de entrevista
 
-- [ ] El precio debería mostrarse al lado del nombre
-- [ ] Mostrar un texto de "Cargando..." hasta que obtengamos los datos
-- [ ] Deberíamos mostrar la cantidad de elementos en el carrito en el botón flotante
-- [ ] No deberíamos poder agregar más de 3 elementos al carrito
+- [x] El precio debería mostrarse al lado del nombre
+- [x] Mostrar un texto de "Cargando..." hasta que obtengamos los datos
+- [x] Deberíamos mostrar la cantidad de elementos en el carrito en el botón flotante
+- [x] No deberíamos poder agregar más de 3 elementos al carrito
--- proyectos-live/tienda-pokemon/caso-0/src/App.tsx
+++ proyectos-live/tienda-pokemon/caso-0/src/App.tsx
@@ -1,10 +1,16 @@
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 
 import api from "./api";
-import {Pokemon} from "./types";
+import { Pokemon } from "./types";
 
 function App() {
   const [pokemons, setPokemons] = useState<Pokemon[]>([]);
+  const [cartItems, setCartItems] = useState<Pokemon[]>([]);
+
+  const handleAddToCart = (pokemon: Pokemon) => {
+    if (cartItems.length < 3)
+      setCartItems((prevItems) => [...prevItems, pokemon]);
+  };
 
   useEffect(() => {
     api.list().then(setPokemons);
@@ -13,19 +19,36 @@ function App() {
   return (
     <>
       <section>
-        {pokemons.map((pokemon) => (
-          <article key={pokemon.id}>
-            <img className="nes-container" src={pokemon.image} />
-            <div>
-              <p>{pokemon.name}</p>
-              <p>{pokemon.description}</p>
-            </div>
-            <button className="nes-btn">Agregar</button>
-          </article>
-        ))}
+        {pokemons.length > 0 ? (
+          pokemons.map((pokemon) => (
+            <article key={pokemon.id}>
+              <img className="nes-container" src={pokemon.image} />
+              <div>
+                <p>
+                  {pokemon.name}{" "}
+                  <span>
+                    {pokemon.price.toLocaleString("en-US", {
+                      style: "currency",
+                      currency: "USD",
+                    })}
+                  </span>
+                </p>
+                <p>{pokemon.description}</p>
+              </div>
+              <button
+                className="nes-btn"
+                onClick={() => handleAddToCart(pokemon)}
+              >
+                Agregar
+              </button>
+            </article>
+          ))
+        ) : (
+          <p>Cargando...</p>
+        )}
       </section>
       <aside>
-        <button className="nes-btn is-primary">0 items</button>
+        <button className="nes-btn is-primary">{cartItems.length} items</button>
       </aside>
     </>
   );
