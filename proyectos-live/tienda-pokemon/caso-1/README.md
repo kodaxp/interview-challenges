d2c0fe96003803c059689e3b1dcaa3ac040897f9
koda <joelzuniga@outlook.cl>
2024-04-29 21:47:52 -0400

    Pokemon shop case 1 resolved

--- proyectos-live/tienda-pokemon/caso-1/README.md
+++ proyectos-live/tienda-pokemon/caso-1/README.md
@@ -1,7 +1,7 @@
 # Ejercicios de entrevista
 
-- [ ] Deberíamos ver el total a pagar en el botón flotante
-- [ ] Nuestro carrito no debería poder exceder los $10
-- [ ] Implementar un buscador por nombre
-- [ ] Deberíamos poder agregar y quitar elementos de favoritos
-- [ ] Los favoritos deberían persistirse al recargar la página
+- [x] Deberíamos ver el total a pagar en el botón flotante
+- [x] Nuestro carrito no debería poder exceder los $10
+- [x] Implementar un buscador por nombre
+- [x] Deberíamos poder agregar y quitar elementos de favoritos
+- [x] Los favoritos deberían persistirse al recargar la página
--- proyectos-live/tienda-pokemon/caso-1/src/App.tsx
+++ proyectos-live/tienda-pokemon/caso-1/src/App.tsx
@@ -5,34 +5,95 @@ import {useState} from "react";
 import {POKEMONS} from "./constants";
 
 function App() {
+  const [pokemons, setPokemons] = useState<Pokemon[]>(POKEMONS);
   const [cart, setCart] = useState<Pokemon[]>([]);
+  const [favorites, setFavorites] = useState<Pokemon[]>(() =>
+    JSON.parse(localStorage.getItem("favorites") || "[]"),
+  );
+
+  const total = cart.reduce((acc, pokemon) => acc + pokemon.price, 0);
+
+  const isInFavorites = (pokemon: Pokemon) => {
+    return favorites.some((pokeFav) => pokeFav.id === pokemon.id);
+  };
+
+  const handleAddToCart = (pokemon: Pokemon) => {
+    if (total + pokemon.price <= 10) setCart((prevState) => [...prevState, pokemon]);
+  };
+
+  const handleAddToFavorites = (pokemon: Pokemon) => {
+    if (isInFavorites(pokemon)) {
+      const newList = favorites.filter((item) => item.id !== pokemon.id);
+
+      localStorage.setItem("favorites", JSON.stringify(newList));
+
+      setFavorites(newList);
+
+      return;
+    }
+    const newList = favorites.concat(pokemon);
+
+    localStorage.setItem("favorites", JSON.stringify(newList));
+
+    setFavorites(newList);
+  };
+
+  const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.value.toLowerCase();
+
+    if (!value) {
+      setPokemons(POKEMONS);
+
+      return;
+    }
+
+    const filteredPokemons = pokemons.filter((pokemon) =>
+      pokemon.name.toLowerCase().includes(value),
+    );
+
+    setPokemons(filteredPokemons);
+  };
 
   return (
     <>
       <nav>
-        <input className="nes-input" id="name_field" placeholder="Charmander" type="text" />
+        <input
+          className="nes-input"
+          id="name_field"
+          placeholder="Charmander"
+          type="text"
+          onChange={handleSearch}
+        />
       </nav>
       <section>
-        {POKEMONS.map((pokemon) => (
-          <article key={pokemon.id}>
-            <figure>
-              <i className="nes-icon is-medium is-transparent heart" />
-              <img className="nes-container" src={pokemon.image} />
-            </figure>
-            <div>
-              <p>
-                {pokemon.name} (${pokemon.price})
-              </p>
-              <p>{pokemon.description}</p>
-            </div>
-            <button className="nes-btn" onClick={() => setCart(cart.concat(pokemon))}>
-              Agregar
-            </button>
-          </article>
-        ))}
+        {pokemons.map((pokemon) => {
+          return (
+            <article key={pokemon.id}>
+              <figure onClick={() => handleAddToFavorites(pokemon)}>
+                <i
+                  className={`nes-icon is-medium heart ${
+                    isInFavorites(pokemon) ? "" : "is-transparent"
+                  }`}
+                />
+                <img className="nes-container" src={pokemon.image} />
+              </figure>
+              <div>
+                <p>
+                  {pokemon.name} (${pokemon.price})
+                </p>
+                <p>{pokemon.description}</p>
+              </div>
+              <button className="nes-btn" onClick={() => handleAddToCart(pokemon)}>
+                Agregar
+              </button>
+            </article>
+          );
+        })}
       </section>
       <aside>
-        <button className="nes-btn is-primary">0 items (total $0)</button>
+        <button className="nes-btn is-primary">
+          {cart.length} items (total ${total})
+        </button>
       </aside>
     </>
   );
